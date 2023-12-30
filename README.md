e5c292058e53089a75ab3d2bc735020b957f01af
ElTorres97 <diegotorres174232@gmail.com>
2023-12-30 12:35:56 -0600

    Streamlit App

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AppSprint5
+En esta Aplicación se realizará la creación y gestión de entornos virtuales de Python, el desarrollo de una aplicación web
+In this App we will create and manage virtual environments of Python, the developments on an App and a service cloud with public access 
---
+++ notebooks/app.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+        
+car_data = pd.read_csv('vehicles_us.csv') # read the data
+
+hist_button = st.checkbox('Build Histogram')
+
+if hist_button:
+    # Create a message
+    st.write('Building a histogram for the odometer column')
+    
+    # Create the histogram
+    fig_hist = px.histogram(car_data, x="odometer")
+    
+    # Show the interactive Plotly chart
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+scatter_button = st.checkbox('Build Scatter Plot')
+
+if scatter_button:
+    # Create a message
+    st.write('Building a scatter plot for the odometer and price columns')
+    
+    # Create the scatter plot
+    fig_scatter = px.scatter(car_data, x="odometer", y="price")
+    
+    # Show the interactive Plotly chart
+    st.plotly_chart(fig_scatter, use_container_width=True)
+
